90683bb5284b354c75afb1c40f4d7b6739e6935e
Sh4dowbolt <suraevvvitaly@gmail.com>
2025-01-15 03:55:00 +0400

    add new module: start kit

---
+++ start_kit/src/main/java/com/suraev/Listener/OnFirstJoinListener.java
@@ -0,0 +1,27 @@
+package com.suraev.Listener;
+
+import de.tr7zw.nbtapi.NBT;
+import de.tr7zw.nbtapi.iface.ReadWriteItemNBT;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OnFirstJoinListener implements Listener {
+
+    @EventHandler
+    public void onPLayerDeath(PlayerJoinEvent event) {
+
+    }
+
+
+}
---
+++ start_kit/src/main/java/com/suraev/start_kit.java
@@ -0,0 +1,23 @@
+package com.suraev;
+
+
+import com.suraev.discord.Command.DiscordCommand;
+import com.suraev.discord.Sheduler.DiscordInformer;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class start_kit extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {System.out.println("Plugin activated");
+       getCommand("discord").setExecutor(new DiscordCommand());
+       Bukkit.getScheduler().runTaskTimerAsynchronously(this, new DiscordInformer(),0,100);
+       getLogger().info(this.getName()+"has been enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info(this.getName()+"has been disabled");
+    }
+}

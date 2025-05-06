5c97cd4dcb6411c1ddf06c6925fdc3cab6058a5e
sh4dowbolt <dmit.palckin@yandex.ru>
2025-05-06 13:11:15 +0400

    local changes

---
+++ clan/src/main/java/com/suraev/ClanRunner.java
@@ -0,0 +1,16 @@
+package com.suraev;
+
+import com.suraev.Entity.Clan;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClanRunner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+       Map<String, List<Clan>> clans = new HashMap<>();
+    }
+}
